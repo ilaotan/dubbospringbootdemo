8fa9ad8c7aad3a364d8c1fa3c56568acafffa767
ilaotan <tanliansheng2011@gmail.com>
2018-04-10 16:52:55 +0800

    remove old

--- consumer/src/main/java/com/ilaotan/DubboConsumerDemo.java
+++ consumer/src/main/java/com/ilaotan/DubboConsumerDemo.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.ilaotan;
 
 import org.springframework.boot.SpringApplication;
@@ -28,13 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-/**
- * Dubbo Consumer Demo
- *
- * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- * @see DemoConsumerController
- * @since 1.0.0
- */
+
 @SpringBootApplication
 @EnableSwagger2
 public class DubboConsumerDemo {
--- provider/src/main/java/com.ilaotan/service/DefaultDemoService.java
+++ provider/src/main/java/com.ilaotan/service/DefaultDemoService.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.ilaotan.service;
 
 import org.slf4j.Logger;
@@ -22,13 +7,7 @@ import org.slf4j.LoggerFactory;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.ilaotan.interfaces.DemoService;
 
-/**
- * Default {@link DemoService}
- *
- * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- * @see DemoService
- * @since 1.0.0
- */
+
 //@Service(interfaceClass = DemoService.class, version = "1.0.0")
 @Service(
         version = "1.0.0",
