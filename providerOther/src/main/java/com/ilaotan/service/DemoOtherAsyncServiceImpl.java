0b6e406c6391ad83bd2711fc94e89927e6d4b3eb
ilaotan <tanliansheng2011@gmail.com>
2018-04-14 11:50:11 +0800

    a lot of code update

---
+++ providerOther/src/main/java/com/ilaotan/service/DemoOtherAsyncServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ilaotan.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.ilaotan.interfaces.IDemoOtherAsyncService;
+
+/**
+ * 类的描述.
+ *
+ * @author tan liansheng on 2018/4/13 15:27
+ */
+@Service(
+        version = "1.0.0"
+        , application = "${dubbo.application.id}"
+        , protocol = "${dubbo.protocol.id}"
+        , registry = "${dubbo.registry.id}"
+)
+public class DemoOtherAsyncServiceImpl implements IDemoOtherAsyncService {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Override
+    public String sayHelloWithSleep(String name) {
+        try {
+            Thread.sleep(6000);
+        }catch (Exception e){
+            logger.error(e.getMessage(), e);
+        }
+        logger.warn("Hello, {}(from Spring Boot sayHelloWithSleep)", name);
+        return "Hello, " + name + " (from Spring Boot sayHelloWithSleep)";
+    }
+
+
+}
--- providerOther/src/main/java/com/ilaotan/service/DemoOtherServiceImpl.java
+++ providerOther/src/main/java/com/ilaotan/service/DemoOtherServiceImpl.java
@@ -4,23 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.dubbo.config.annotation.Service;
-import com.ilaotan.interfaces.IDemo2Service;
+import com.ilaotan.interfaces.IDemoOtherService;
 
 /**
  * 类的描述.
  *
- * @author tan liansheng on 2018/4/13 15:27
+ * @author tan liansheng on 2018/4/14 11:44
  */
+
 @Service(
         version = "1.0.0"
         , application = "${dubbo.application.id}"
         , protocol = "${dubbo.protocol.id}"
         , registry = "${dubbo.registry.id}"
 )
-public class Demo2ServiceImpl implements IDemo2Service {
+public class DemoOtherServiceImpl implements IDemoOtherService{
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-
 
     @Override
     public String sayHelloWithSleep(String name) {
@@ -32,6 +32,5 @@ public class Demo2ServiceImpl implements IDemo2Service {
         logger.warn("Hello, {}(from Spring Boot sayHelloWithSleep)", name);
         return "Hello, " + name + " (from Spring Boot sayHelloWithSleep)";
     }
-
 
 }
